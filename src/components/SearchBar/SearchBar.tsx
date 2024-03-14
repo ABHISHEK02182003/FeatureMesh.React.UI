11dc753a6981e371e56bdec9c2e96bba6bb17825
Nayek <sankhanil.nayek.ext@zeiss.com>
2024-03-14 15:32:23 +0530

    Updated API endpoints

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,9 +1,38 @@
 // SearchPage.tsx
-
+import { useState, useEffect } from 'react';
 import React from 'react';
 import styles from './SearchBar.module.css';
 
 const SearchPage: React.FC = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [featureResults, setFeatureResults] = useState([]);
+  const [entityResults, setEntityResults] = useState([]);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const fetchData = async () => {
+    const featureUrl = `https://featuremesharch.azurewebsites.net/search?type=Feature&name=${searchQuery}`;
+    const entityUrl = `https://featuremesharch.azurewebsites.net/search?type=Entity&name=${searchQuery}`;
+
+    const featureResponse = await fetch(featureUrl);
+    const featureData = await featureResponse.json();
+    setFeatureResults(featureData);
+
+    const entityResponse = await fetch(entityUrl);
+    const entityData = await entityResponse.json();
+    setEntityResults(entityData);
+
+    if (featureResults) {
+      console.log("Searched");
+    }
+  };
+
+  const handleSearchClick = () => {
+    fetchData();
+  };
+
   return (
     <div className={styles.searchPage}>
       <div className={styles.searchContainer}>
@@ -11,6 +40,8 @@ const SearchPage: React.FC = () => {
           type="text"
           className={styles.searchInput}
           placeholder="Search by Entity, Feature, or Author"
+          value={searchQuery}
+          onChange={handleInputChange}
         />
         <button className={styles.searchButton}>Search</button>
       </div>
